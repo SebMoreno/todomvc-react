ad1ba6299b28650031d107401eb72422dc3d915b
Sebastián <juansebasmorenoruiz@gmail.com>
2023-09-16 11:14:52 -0500

    create todo component

--- src/pages/todo-app/Todo.tsx
+++ src/pages/todo-app/Todo.tsx
@@ -1,21 +1,73 @@
-import { FC, useState } from "react";
-import { Todo } from "../../types.ts";
+import React, { FC, useEffect, useRef, useState } from 'react';
+import { Todo as TodoType } from "../../types.ts";
 
 interface TodoProps {
-    id: Todo["id"];
-    completed: Todo["completed"];
-    title: Todo["title"];
+    completed: TodoType["completed"];
+    title: TodoType["title"];
+    isEditing: boolean;
+    onEditingChange: (isEditing: boolean) => void;
+    onCompleted: (completed: TodoType["completed"]) => void;
+    onChangeTitle: (title: TodoType["title"]) => void;
+    onDelete: () => void;
 }
 
+export const Todo: FC<TodoProps> = ({
+                                        completed,
+                                        title,
+                                        isEditing,
+                                        onEditingChange,
+                                        onCompleted,
+                                        onChangeTitle,
+                                        onDelete
+                                    }) => {
+    const [titleEdited, setTitleEdited] = useState(title);
+    const inputEditTitle = useRef<HTMLInputElement>(null);
+    useEffect(() => {
+        if (isEditing) {
+            inputEditTitle.current?.focus();
+        } else {
+            inputEditTitle.current?.blur();
+        }
+    }, [isEditing]);
 
-export const Todo: FC<TodoProps> = ({title, completed, id}) => {
+    const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = e => {
+        if (e.key === 'Enter') {
+            const newTitle = titleEdited.trim();
+            setTitleEdited(newTitle);
+            onEditingChange(false);
+            if (newTitle !== title) {
+                onChangeTitle(titleEdited);
+            }
+            if (newTitle === '') {
+                onDelete();
+            }
+        }
+        if (e.key === 'Escape') {
+            setTitleEdited(title);
+            onEditingChange(false);
+        }
+    };
 
-    return <>
-        <div className="view">
-            <input type="checkbox" className="toggle" checked={completed}/>
-            <label htmlFor="">{title}</label>
-            <button className="destroy"/>
-        </div>
-        <input type="text" className="edit"/>
-    </>;
+    return (
+        <>
+            <div className="view">
+                <input
+                    className="toggle"
+                    type="checkbox"
+                    checked={completed}
+                    onChange={e => onCompleted(e.target.checked)}
+                />
+                <label onDoubleClick={() => onEditingChange(true)}>{title}</label>
+                <button className="destroy" onClick={() => onDelete()}/>
+            </div>
+            <input
+                className="edit"
+                ref={inputEditTitle}
+                value={titleEdited}
+                onKeyDown={handleKeyDown}
+                onChange={e => setTitleEdited(e.target.value)}
+                onBlur={() => onEditingChange(false)}
+            />
+        </>
+    );
 };
--- src/pages/todo-app/TodoPage.tsx
+++ src/pages/todo-app/TodoPage.tsx
@@ -2,9 +2,24 @@ import 'todomvc-app-css/index.css';
 import { Todo } from "./Todo.tsx";
 import { useState } from "react";
 import { mockTodos } from "../../mocks/todos.ts";
+import { Todo as TodoType } from "../../types.ts";
 
 export const TodoPage = () => {
     const [todos, setTodos] = useState(mockTodos);
+    const [idTodoEdited, setIdTodoEdited] = useState<TodoType["id"] | null>(null);
+
+    function handleTodoCompleted(id: TodoType["id"], completed: TodoType["completed"]) {
+        setTodos(todos.map(todo => todo.id !== id ? todo : {...todo, completed}));
+    }
+
+    function handleChangeTodoTitle(id: TodoType["id"], title: TodoType["title"]) {
+        setTodos(todos.map(todo => todo.id !== id ? todo : {...todo, title}));
+    }
+
+    function handleDeleteTodo(id: TodoType["id"]) {
+        setTodos(todos.filter(todo => todo.id !== id));
+    }
+
 
     return <main className="todoapp">
         <header className="header">
@@ -12,8 +27,20 @@ export const TodoPage = () => {
             <input className="new-todo" placeholder="¿Qué quieres hacer?" autoFocus/>
         </header>
         <ul className="todo-list">
-            {todos.map(todo => <li className="" key={todo.id}>
-                <Todo id={todo.id} completed={todo.completed} title={todo.title}/>
+            {todos.map(todo =>
+                <li className={`
+                        ${todo.completed ? 'completed' : ''}
+                        ${idTodoEdited === todo.id ? 'editing' : ''}
+                     `} key={todo.id}>
+                <Todo
+                    completed={todo.completed}
+                    title={todo.title}
+                    isEditing={idTodoEdited === todo.id}
+                    onEditingChange={isEditing => setIdTodoEdited(isEditing ? todo.id : null)}
+                    onCompleted={completed => handleTodoCompleted(todo.id, completed)}
+                    onChangeTitle={title => handleChangeTodoTitle(todo.id, title)}
+                    onDelete={() => handleDeleteTodo(todo.id)}
+                />
             </li>)}
         </ul>
         <footer className="footer">
